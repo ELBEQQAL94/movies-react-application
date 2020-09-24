da1e0cd9dc42d51ad12ce70de8ad68b2922b5e23
ELBEQQAL94 <ELBEQQAL.youssef@gmail.com>
2020-09-24 17:25:01 +0200

    generate routes dynamicly

--- app/src/AppRoute.js
+++ app/src/AppRoute.js
@@ -20,8 +20,8 @@ const AppRoute = ({
 };
 
 AppRoute.propTypes = {
-    component: PropTypes.node.isRequired,
-    layout: PropTypes.node.isRequired,
+    component: PropTypes.func.isRequired,
+    layout: PropTypes.func.isRequired,
 };
 
 export default AppRoute;
