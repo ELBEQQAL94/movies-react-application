f0ac926ab31cdca775195f6014f0effbc2b93f64
ELBEQQAL94 <ELBEQQAL.youssef@gmail.com>
2020-09-28 18:07:11 +0100

    add collapse component

--- app/src/components/collapse/Collapse.js
+++ app/src/components/collapse/Collapse.js
@@ -2,16 +2,14 @@ import React from 'react';
 
 const Collapse = ({children}) => {
     return (
-        <>
-            <p>
-                <button className="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                    Filter
-                </button>
-            </p>
+        <div className="mb-4">
+            <button className="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                Filter
+            </button>
             <div className="collapse" id="collapseExample">
                 {children}
             </div>
-        </>
+        </div>
     );
 };
 
